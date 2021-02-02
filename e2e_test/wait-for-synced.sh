3d91047d961bcbbf15d9a5ea4da0e2a34c918f42
Hidetake Iwata <int128@gmail.com>
2021-02-02 19:30:54 +0900

    Update wait-for-synced.sh

--- e2e_test/wait-for-synced.sh
+++ e2e_test/wait-for-synced.sh
@@ -5,7 +5,7 @@ set -eux
 application_name="$1"
 want_revision="$(git rev-parse HEAD)"
 
-for (( i = 0; i < 10; i++ )); do
+for (( i = 0; i < 15; i++ )); do
   status="$(kubectl -n argocd get application "$application_name" '-ojsonpath={.status.sync.status}/{.status.sync.revision}')"
   if [[ $status == Synced/$want_revision ]]; then
     exit 0
