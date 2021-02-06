8537692ee32ff3c700255464eb8695319a51ee6d
Hidetake Iwata <int128@gmail.com>
2021-02-06 12:14:22 +0900

    Decrease log verbosity of wait-for-synced.sh

--- e2e_test/wait-for-synced.sh
+++ e2e_test/wait-for-synced.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 set -o pipefail
-set -eux
+set -eu
 
 application_name="$1"
 want_revision="$(git rev-parse HEAD)"
 
 for (( i = 0; i < 15; i++ )); do
   status="$(kubectl -n argocd get application "$application_name" '-ojsonpath={.status.sync.status}/{.status.sync.revision}')"
+  echo "[wait-for-synced] got:  $status"
+  echo "[wait-for-synced] want: Synced/$want_revision"
   if [[ $status == Synced/$want_revision ]]; then
     exit 0
   fi
+  echo "[wait-for-synced] retry after ${i}s"
   sleep "$i"
 done
 exit 1
