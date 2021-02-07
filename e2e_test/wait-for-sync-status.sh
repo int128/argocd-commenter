33abaeb06313533df1ddfc5a391b09936755c9f7
Hidetake Iwata <int128@gmail.com>
2021-02-07 16:38:07 +0900

    Change timeout of wait sync

--- e2e_test/wait-for-sync-status.sh
+++ e2e_test/wait-for-sync-status.sh
@@ -8,7 +8,7 @@ want_revision="$(git rev-parse "$want_branch")"
 want_sync_status="$3"
 want_phase="$4"
 
-for (( i = 0; i < 20; i++ )); do
+for (( i = 0; i < 10; i++ )); do
   got="$(kubectl -n argocd get application "$application_name" '-ojsonpath={.status.sync.status}/{.status.operationState.phase}/{.status.sync.revision}')"
   want="$want_sync_status/$want_phase/$want_revision"
   echo "[wait-for-sync-status] got:  $got"
