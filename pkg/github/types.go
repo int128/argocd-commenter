82adfadfaeb3ea3fbd717ecb6399f6a7c30eeab6
Hidetake Iwata <int128@gmail.com>
2021-09-05 16:14:06 +0900

    Remove #284

--- pkg/github/types.go
+++ pkg/github/types.go
@@ -3,8 +3,6 @@ package github
 import (
 	"fmt"
 	"net/url"
-	"regexp"
-	"strconv"
 	"strings"
 )
 
@@ -26,28 +24,4 @@ func ParseRepositoryURL(urlstr string) (*Repository, error) {
 		return nil, fmt.Errorf("invalid path %s", u.Path)
 	}
 	return &Repository{Owner: c[0], Name: c[1]}, nil
-}
-
-var pullRequestPattern = regexp.MustCompile(`https://github\.com/([\w-]+?)/([\w-]+?)/pull/(\d+)`)
-
-type PullRequest struct {
-	Repository
-	Number int
-}
-
-func FindPullRequestURLs(s string) []PullRequest {
-	matches := pullRequestPattern.FindAllStringSubmatch(s, -1)
-	if matches == nil {
-		return nil
-	}
-	var p []PullRequest
-	for _, m := range matches {
-		owner, name, number := m[1], m[2], m[3]
-		n, err := strconv.Atoi(number)
-		if err != nil {
-			continue
-		}
-		p = append(p, PullRequest{Repository: Repository{Owner: owner, Name: name}, Number: n})
-	}
-	return p
 }
