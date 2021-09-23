bd6d95944c316adf09694cc73ac9a74ce7128626
ci <ci@example.com>
2021-09-23 04:27:51 +0000

    Initial commit

---
+++ e2e_test/controller/create-controller-manager-secret.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -o pipefail
+set -e
+
+kubectl -n argocd-commenter-system delete secret controller-manager || true
+
+# for personal access token
+if [ "$GITHUB_TOKEN" ]; then
+  echo 'using GITHUB_TOKEN'
+  kubectl -n argocd-commenter-system create secret generic controller-manager \
+    --from-literal="GITHUB_TOKEN=$GITHUB_TOKEN"
+  exit 0
+fi
+
+# for installation access token
+if [ "$GITHUB_APP_ID" ]; then
+  echo 'using GITHUB_APP_ID'
+  kubectl -n argocd-commenter-system create secret generic controller-manager \
+    --from-literal="GITHUB_APP_ID=$GITHUB_APP_ID" \
+    --from-literal="GITHUB_APP_INSTALLATION_ID=$GITHUB_APP_INSTALLATION_ID" \
+    --from-file="GITHUB_APP_PRIVATE_KEY=$GITHUB_APP_PRIVATE_KEY_PATH"
+  exit 0
+fi
+
+echo 'you need to set either GITHUB_TOKEN or GITHUB_APP_ID' >&2
+exit 1
