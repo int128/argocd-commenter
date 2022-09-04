ab3cb1aad2355e26abeec4ce8b56461710d415c7
Hidetake Iwata <int128@gmail.com>
2022-09-04 09:00:14 +0000

    Extend timeout

--- e2e_test/wait-for-sync-status.sh
+++ e2e_test/wait-for-sync-status.sh
@@ -9,7 +9,7 @@ want_sync_status="$3"
 want_phase="$4"
 want_health_status="$5"
 
-for (( i = 0; i < 10; i++ )); do
+for (( i = 0; i < 30; i++ )); do
   got="$(kubectl -n argocd get application "$application_name" '-ojsonpath={.status.sync.status}/{.status.operationState.phase}/{.status.health.status}/{.status.sync.revision}')"
   want="$want_sync_status/$want_phase/$want_health_status/$want_revision"
   echo "[wait-for-sync-status] got:  $got"
