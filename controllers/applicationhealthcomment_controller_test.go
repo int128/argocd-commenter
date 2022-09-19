87f53bf0a778308730fbf9d75955f7cd499a74aa
Hidetake Iwata <int128@gmail.com>
2022-09-19 19:01:17 +0900

    refactor: use generateName and patch update (#718)
    * refactor: use generateName and patch update
    
    * refactor: extract BeforeEach

--- controllers/applicationhealthcomment_controller_test.go
+++ controllers/applicationhealthcomment_controller_test.go
@@ -9,92 +9,83 @@ import (
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("Application health comment controller", func() {
 	const timeout = time.Second * 3
 	const interval = time.Millisecond * 250
-	appKey := types.NamespacedName{Namespace: "default", Name: "app2"}
+	var app argocdv1alpha1.Application
+	var appKey types.NamespacedName
+
+	BeforeEach(func() {
+		app = argocdv1alpha1.Application{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "argoproj.io/v1alpha1",
+				Kind:       "Application",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "fixture-",
+				Namespace:    "default",
+			},
+			Spec: argocdv1alpha1.ApplicationSpec{
+				Project: "default",
+				Source: argocdv1alpha1.ApplicationSource{
+					RepoURL:        "https://github.com/int128/argocd-commenter.git",
+					Path:           "test",
+					TargetRevision: "main",
+				},
+				Destination: argocdv1alpha1.ApplicationDestination{
+					Server:    "https://kubernetes.default.svc",
+					Namespace: "default",
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, &app)).Should(Succeed())
+		appKey = types.NamespacedName{Namespace: app.Namespace, Name: app.Name}
+	})
 
 	Context("When an application is healthy", func() {
-		It("Should notify a comment", func() {
-			By("By creating an application")
-			Expect(k8sClient.Create(ctx, &argocdv1alpha1.Application{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "argoproj.io/v1alpha1",
-					Kind:       "Application",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      appKey.Name,
-					Namespace: appKey.Namespace,
-				},
-				Spec: argocdv1alpha1.ApplicationSpec{
-					Project: "default",
-					Source: argocdv1alpha1.ApplicationSource{
-						RepoURL:        "https://github.com/int128/argocd-commenter.git",
-						Path:           "test",
-						TargetRevision: "main",
-					},
-					Destination: argocdv1alpha1.ApplicationDestination{
-						Server:    "https://kubernetes.default.svc",
-						Namespace: "default",
-					},
-				},
-			})).Should(Succeed())
-
+		It("Should notify a comment once", func() {
 			By("By updating the health status to progressing")
-			Eventually(func(g Gomega) {
-				var app argocdv1alpha1.Application
-				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
-				app.Status = argocdv1alpha1.ApplicationStatus{
-					Health: argocdv1alpha1.HealthStatus{
-						Status: health.HealthStatusProgressing,
-					},
-					OperationState: &argocdv1alpha1.OperationState{
-						StartedAt: metav1.Now(),
-						Operation: argocdv1alpha1.Operation{
-							Sync: &argocdv1alpha1.SyncOperation{
-								Revision: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-							},
+			patch := client.MergeFrom(app.DeepCopy())
+			app.Status = argocdv1alpha1.ApplicationStatus{
+				Health: argocdv1alpha1.HealthStatus{
+					Status: health.HealthStatusProgressing,
+				},
+				OperationState: &argocdv1alpha1.OperationState{
+					StartedAt: metav1.Now(),
+					Operation: argocdv1alpha1.Operation{
+						Sync: &argocdv1alpha1.SyncOperation{
+							Revision: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 						},
 					},
-				}
-				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
-			}, timeout, interval).Should(Succeed())
+				},
+			}
+			Expect(k8sClient.Patch(ctx, &app, patch)).Should(Succeed())
 
 			By("By updating the health status to healthy")
-			Eventually(func(g Gomega) {
-				var app argocdv1alpha1.Application
-				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
-				app.Status.Health.Status = health.HealthStatusHealthy
-				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
-			}, timeout, interval).Should(Succeed())
+			patch = client.MergeFrom(app.DeepCopy())
+			app.Status.Health.Status = health.HealthStatusHealthy
+			Expect(k8sClient.Patch(ctx, &app, patch)).Should(Succeed())
 
 			Eventually(func() int {
 				return notificationMock.Comments.CountBy(appKey)
 			}, timeout, interval).Should(Equal(1))
-		})
 
-		It("Should not notify a comment after healthy", func() {
 			By("By updating the health status to progressing")
-			Eventually(func(g Gomega) {
-				var app argocdv1alpha1.Application
-				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
-				app.Status.Health.Status = health.HealthStatusHealthy
-				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
-			}, timeout, interval).Should(Succeed())
+			patch = client.MergeFrom(app.DeepCopy())
+			app.Status.Health.Status = health.HealthStatusProgressing
+			Expect(k8sClient.Patch(ctx, &app, patch)).Should(Succeed())
 
 			By("By updating the health status to healthy")
-			Eventually(func(g Gomega) {
-				var app argocdv1alpha1.Application
-				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
-				app.Status.Health.Status = health.HealthStatusHealthy
-				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
-			}, timeout, interval).Should(Succeed())
+			patch = client.MergeFrom(app.DeepCopy())
+			app.Status.Health.Status = health.HealthStatusHealthy
+			Expect(k8sClient.Patch(ctx, &app, patch)).Should(Succeed())
 
 			Consistently(func() int {
 				return notificationMock.Comments.CountBy(appKey)
-			}, 1*time.Second, interval).Should(Equal(1))
+			}, 100*time.Millisecond).Should(Equal(1))
 		})
 	})
 })
--- controllers/applicationphase_controller_test.go
+++ controllers/applicationphase_controller_test.go
@@ -9,56 +9,58 @@ import (
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("Application phase controller", func() {
 	const timeout = time.Second * 3
 	const interval = time.Millisecond * 250
-	appKey := types.NamespacedName{Namespace: "default", Name: "app1"}
+	var app argocdv1alpha1.Application
+	var appKey types.NamespacedName
+
+	BeforeEach(func() {
+		app = argocdv1alpha1.Application{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "argoproj.io/v1alpha1",
+				Kind:       "Application",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "fixture-",
+				Namespace:    "default",
+			},
+			Spec: argocdv1alpha1.ApplicationSpec{
+				Project: "default",
+				Source: argocdv1alpha1.ApplicationSource{
+					RepoURL:        "https://github.com/int128/argocd-commenter.git",
+					Path:           "test",
+					TargetRevision: "main",
+				},
+				Destination: argocdv1alpha1.ApplicationDestination{
+					Server:    "https://kubernetes.default.svc",
+					Namespace: "default",
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, &app)).Should(Succeed())
+		appKey = types.NamespacedName{Namespace: app.Namespace, Name: app.Name}
+	})
 
 	Context("When an application is synced", func() {
 		It("Should notify a comment and deployment status", func() {
-			By("By creating an application")
-			Expect(k8sClient.Create(ctx, &argocdv1alpha1.Application{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "argoproj.io/v1alpha1",
-					Kind:       "Application",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      appKey.Name,
-					Namespace: appKey.Namespace,
-				},
-				Spec: argocdv1alpha1.ApplicationSpec{
-					Project: "default",
-					Source: argocdv1alpha1.ApplicationSource{
-						RepoURL:        "https://github.com/int128/argocd-commenter.git",
-						Path:           "test/app1",
-						TargetRevision: "main",
-					},
-					Destination: argocdv1alpha1.ApplicationDestination{
-						Server:    "https://kubernetes.default.svc",
-						Namespace: "default",
-					},
-				},
-			})).Should(Succeed())
-
 			By("By updating the operation state to running")
-			Eventually(func(g Gomega) {
-				var app argocdv1alpha1.Application
-				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
-				app.Status = argocdv1alpha1.ApplicationStatus{
-					OperationState: &argocdv1alpha1.OperationState{
-						Phase:     synccommon.OperationRunning,
-						StartedAt: metav1.Now(),
-						Operation: argocdv1alpha1.Operation{
-							Sync: &argocdv1alpha1.SyncOperation{
-								Revision: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-							},
+			patch := client.MergeFrom(app.DeepCopy())
+			app.Status = argocdv1alpha1.ApplicationStatus{
+				OperationState: &argocdv1alpha1.OperationState{
+					Phase:     synccommon.OperationRunning,
+					StartedAt: metav1.Now(),
+					Operation: argocdv1alpha1.Operation{
+						Sync: &argocdv1alpha1.SyncOperation{
+							Revision: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 						},
 					},
-				}
-				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
-			}, timeout, interval).Should(Succeed())
+				},
+			}
+			Expect(k8sClient.Patch(ctx, &app, patch)).Should(Succeed())
 
 			Eventually(func() int {
 				return notificationMock.Comments.CountBy(appKey)
@@ -68,12 +70,9 @@ var _ = Describe("Application phase controller", func() {
 			}, timeout, interval).Should(Equal(1))
 
 			By("By updating the operation state to succeeded")
-			Eventually(func(g Gomega) {
-				var app argocdv1alpha1.Application
-				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
-				app.Status.OperationState.Phase = synccommon.OperationSucceeded
-				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
-			}, timeout, interval).Should(Succeed())
+			patch = client.MergeFrom(app.DeepCopy())
+			app.Status.OperationState.Phase = synccommon.OperationSucceeded
+			Expect(k8sClient.Patch(ctx, &app, patch)).Should(Succeed())
 
 			Eventually(func() int {
 				return notificationMock.Comments.CountBy(appKey)
