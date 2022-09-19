cb90e19530daedf0e928ff5b5704a56ef633752f
Hidetake Iwata <int128@gmail.com>
2022-09-19 14:41:15 +0900

    refactor: add test for ApplicationHealthCommentReconciler

--- controllers/applicationhealthcomment_controller_test.go
+++ controllers/applicationhealthcomment_controller_test.go
@@ -5,24 +5,24 @@ import (
 	"time"
 
 	argocdv1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
-	synccommon "github.com/argoproj/gitops-engine/pkg/sync/common"
+	"github.com/argoproj/gitops-engine/pkg/health"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
-var _ = Describe("Application phase controller", func() {
-	const timeout = time.Second * 10
+var _ = Describe("Application health comment controller", func() {
+	const timeout = time.Second * 3
 	const interval = time.Millisecond * 250
 
-	Context("When an application is synced", func() {
-		It("Should notify a comment and deployment status", func() {
+	Context("When an application is healthy", func() {
+		It("Should notify a comment", func() {
 			ctx, cancel := context.WithCancel(ctx)
 			defer cancel()
 
 			By("By creating an application")
-			appKey := types.NamespacedName{Namespace: "default", Name: "app1"}
+			appKey := types.NamespacedName{Namespace: "default", Name: "app2"}
 			Expect(k8sClient.Create(ctx, &argocdv1alpha1.Application{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "argoproj.io/v1alpha1",
@@ -36,7 +36,7 @@ var _ = Describe("Application phase controller", func() {
 					Project: "default",
 					Source: argocdv1alpha1.ApplicationSource{
 						RepoURL:        "https://github.com/int128/argocd-commenter.git",
-						Path:           "test/app1",
+						Path:           "test",
 						TargetRevision: "main",
 					},
 					Destination: argocdv1alpha1.ApplicationDestination{
@@ -46,13 +46,15 @@ var _ = Describe("Application phase controller", func() {
 				},
 			})).Should(Succeed())
 
-			By("By updating the operation state to running")
+			By("By updating the health status to progressing")
 			Eventually(func(g Gomega) {
 				var app argocdv1alpha1.Application
 				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
 				app.Status = argocdv1alpha1.ApplicationStatus{
+					Health: argocdv1alpha1.HealthStatus{
+						Status: health.HealthStatusProgressing,
+					},
 					OperationState: &argocdv1alpha1.OperationState{
-						Phase:     synccommon.OperationRunning,
 						StartedAt: metav1.Now(),
 						Operation: argocdv1alpha1.Operation{
 							Sync: &argocdv1alpha1.SyncOperation{
@@ -64,21 +66,35 @@ var _ = Describe("Application phase controller", func() {
 				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
 			}, timeout, interval).Should(Succeed())
 
-			Eventually(func() int {
-				return notificationMock.Comments.CountBy(appKey)
-			}, timeout, interval).Should(Equal(1))
-
-			By("By updating the operation state to succeeded")
+			By("By updating the health status to healthy")
 			Eventually(func(g Gomega) {
 				var app argocdv1alpha1.Application
 				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
-				app.Status.OperationState.Phase = synccommon.OperationSucceeded
+				app.Status.Health.Status = health.HealthStatusHealthy
 				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
 			}, timeout, interval).Should(Succeed())
 
 			Eventually(func() int {
 				return notificationMock.Comments.CountBy(appKey)
-			}, timeout, interval).Should(Equal(2))
+			}, timeout, interval).Should(Equal(1))
+
+			By("By updating the health status to progressing")
+			Eventually(func(g Gomega) {
+				var app argocdv1alpha1.Application
+				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
+				app.Status.Health.Status = health.HealthStatusHealthy
+				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
+			}, timeout, interval).Should(Succeed())
+
+			By("By updating the health status to healthy")
+			Eventually(func(g Gomega) {
+				var app argocdv1alpha1.Application
+				g.Expect(k8sClient.Get(ctx, appKey, &app)).Should(Succeed())
+				app.Status.Health.Status = health.HealthStatusHealthy
+				g.Expect(k8sClient.Update(ctx, &app)).Should(Succeed())
+			}, timeout, interval).Should(Succeed())
+
+			Expect(notificationMock.Comments.CountBy(appKey)).Should(Equal(1))
 		})
 	})
 })
