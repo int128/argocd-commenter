e5ff843d28ab57bb33bf78692161c70de59231b4
Hidetake Iwata <int128@gmail.com>
2022-09-19 18:58:17 +0900

    refactor: extract BeforeEach

--- controllers/applicationhealthdeployment_controller_test.go
+++ controllers/applicationhealthdeployment_controller_test.go
@@ -15,38 +15,41 @@ import (
 var _ = Describe("Application health deployment controller", func() {
 	const timeout = time.Second * 3
 	const interval = time.Millisecond * 250
+	var app argocdv1alpha1.Application
+	var appKey types.NamespacedName
+
+	BeforeEach(func() {
+		app = argocdv1alpha1.Application{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "argoproj.io/v1alpha1",
+				Kind:       "Application",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "fixture-",
+				Namespace:    "default",
+				Annotations: map[string]string{
+					"argocd-commenter.int128.github.io/deployment-url": "https://api.github.com/repos/int128/argocd-commenter/deployments/1234567890",
+				},
+			},
+			Spec: argocdv1alpha1.ApplicationSpec{
+				Project: "default",
+				Source: argocdv1alpha1.ApplicationSource{
+					RepoURL:        "https://github.com/int128/argocd-commenter.git",
+					Path:           "test",
+					TargetRevision: "main",
+				},
+				Destination: argocdv1alpha1.ApplicationDestination{
+					Server:    "https://kubernetes.default.svc",
+					Namespace: "default",
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, &app)).Should(Succeed())
+		appKey = types.NamespacedName{Namespace: app.Namespace, Name: app.Name}
+	})
 
 	Context("When an application is healthy", func() {
 		It("Should notify a deployment status once", func() {
-			By("By creating an application")
-			app := argocdv1alpha1.Application{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "argoproj.io/v1alpha1",
-					Kind:       "Application",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					GenerateName: "fixture-",
-					Namespace:    "default",
-					Annotations: map[string]string{
-						"argocd-commenter.int128.github.io/deployment-url": "https://api.github.com/repos/int128/argocd-commenter/deployments/1234567890",
-					},
-				},
-				Spec: argocdv1alpha1.ApplicationSpec{
-					Project: "default",
-					Source: argocdv1alpha1.ApplicationSource{
-						RepoURL:        "https://github.com/int128/argocd-commenter.git",
-						Path:           "test",
-						TargetRevision: "main",
-					},
-					Destination: argocdv1alpha1.ApplicationDestination{
-						Server:    "https://kubernetes.default.svc",
-						Namespace: "default",
-					},
-				},
-			}
-			Expect(k8sClient.Create(ctx, &app)).Should(Succeed())
-			appKey := types.NamespacedName{Namespace: app.Namespace, Name: app.Name}
-
 			By("By updating the health status to progressing")
 			patch := client.MergeFrom(app.DeepCopy())
 			app.Status = argocdv1alpha1.ApplicationStatus{
--- controllers/applicationphase_controller_test.go
+++ controllers/applicationphase_controller_test.go
@@ -15,35 +15,38 @@ import (
 var _ = Describe("Application phase controller", func() {
 	const timeout = time.Second * 3
 	const interval = time.Millisecond * 250
+	var app argocdv1alpha1.Application
+	var appKey types.NamespacedName
+
+	BeforeEach(func() {
+		app = argocdv1alpha1.Application{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "argoproj.io/v1alpha1",
+				Kind:       "Application",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "fixture-",
+				Namespace:    "default",
+			},
+			Spec: argocdv1alpha1.ApplicationSpec{
+				Project: "default",
+				Source: argocdv1alpha1.ApplicationSource{
+					RepoURL:        "https://github.com/int128/argocd-commenter.git",
+					Path:           "test",
+					TargetRevision: "main",
+				},
+				Destination: argocdv1alpha1.ApplicationDestination{
+					Server:    "https://kubernetes.default.svc",
+					Namespace: "default",
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, &app)).Should(Succeed())
+		appKey = types.NamespacedName{Namespace: app.Namespace, Name: app.Name}
+	})
 
 	Context("When an application is synced", func() {
 		It("Should notify a comment and deployment status", func() {
-			By("By creating an application")
-			app := argocdv1alpha1.Application{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "argoproj.io/v1alpha1",
-					Kind:       "Application",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					GenerateName: "fixture-",
-					Namespace:    "default",
-				},
-				Spec: argocdv1alpha1.ApplicationSpec{
-					Project: "default",
-					Source: argocdv1alpha1.ApplicationSource{
-						RepoURL:        "https://github.com/int128/argocd-commenter.git",
-						Path:           "test/app1",
-						TargetRevision: "main",
-					},
-					Destination: argocdv1alpha1.ApplicationDestination{
-						Server:    "https://kubernetes.default.svc",
-						Namespace: "default",
-					},
-				},
-			}
-			Expect(k8sClient.Create(ctx, &app)).Should(Succeed())
-			appKey := types.NamespacedName{Namespace: app.Namespace, Name: app.Name}
-
 			By("By updating the operation state to running")
 			patch := client.MergeFrom(app.DeepCopy())
 			app.Status = argocdv1alpha1.ApplicationStatus{
