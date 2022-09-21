41cb2ec1c892c953d62ba8eabed7be73d08e7ded
Hidetake Iwata <int128@gmail.com>
2022-09-21 19:14:19 +0900

    Fix deployment did not become Active for CronJob

--- pkg/notification/deployment.go
+++ pkg/notification/deployment.go
@@ -58,6 +58,12 @@ func generateDeploymentStatus(e Event) *github.DeploymentStatus {
 			ds.State = "queued"
 			return &ds
 		case synccommon.OperationSucceeded:
+			// Some resources (such as CronJob) do not trigger Progressing status.
+			// If healthy, complete the deployment as success.
+			if e.Application.Status.Health.Status == health.HealthStatusHealthy {
+				ds.State = "success"
+				return &ds
+			}
 			ds.State = "in_progress"
 			return &ds
 		case synccommon.OperationFailed:
