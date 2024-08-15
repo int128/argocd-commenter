43bba6d3a9a5ad3245d40f28076655701a8e6c68
Hidetake Iwata <int128@gmail.com>
2024-08-15 15:31:58 +0900

    Refactor e2e test (#1122)
    * Refactor e2e test
    
    * Refactor

---
+++ e2e_test/fixture/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eux -o pipefail
+
+: "$GITHUB_REPOSITORY"
+: "$GITHUB_REF_NAME"
+: "$GITHUB_RUN_NUMBER"
+
+app="$1"
+head_branch="e2e-test/$GITHUB_RUN_NUMBER/deploy-$app"
+base_branch="e2e-test/$GITHUB_RUN_NUMBER/main"
+
+# Commit the changes
+echo "deploymentURL: $DEPLOYMENT_URL" > "$app/metadata.yaml"
+git checkout -b "$head_branch"
+git add .
+git commit -m "e2e-test: Update $app"
+
+# Push and merge it
+git push origin -f "$head_branch"
+gh pr create --base "$base_branch" --fill --body "$GITHUB_REPOSITORY#${GITHUB_REF_NAME%%/*}" --label e2e-test
+gh pr merge --squash
+
+# Reset the local state
+git checkout "$base_branch"
+git pull origin "$base_branch" --ff-only
