47f0cc650c00d04144431cab54318080615dbbb8
Hidetake Iwata <int128@gmail.com>
2024-08-16 14:27:57 +0900

    Create commit comment if no related pull request (#1129)

--- internal/github/comment.go
+++ internal/github/comment.go
@@ -13,11 +13,20 @@ type Comment struct {
 	Body       string
 }
 
-func (c *client) CreateComment(ctx context.Context, r Repository, pullNumber int, body string) error {
+func (c *client) CreatePullRequestComment(ctx context.Context, r Repository, pullNumber int, body string) error {
 	_, _, err := c.rest.Issues.CreateComment(ctx, r.Owner, r.Name, pullNumber,
 		&github.IssueComment{Body: github.String(body)})
 	if err != nil {
 		return fmt.Errorf("could not create a comment to the pull request #%d: %w", pullNumber, err)
+	}
+	return nil
+}
+
+func (c *client) CreateCommitComment(ctx context.Context, r Repository, sha, body string) error {
+	_, _, err := c.rest.Repositories.CreateComment(ctx, r.Owner, r.Name, sha,
+		&github.RepositoryComment{Body: github.String(body)})
+	if err != nil {
+		return fmt.Errorf("could not create a comment to the commit %s: %w", sha, err)
 	}
 	return nil
 }
