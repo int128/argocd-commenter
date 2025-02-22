ab043b172725cca6910a320859c032964bf81148
Hidetake Iwata <int128@gmail.com>
2025-02-22 15:31:13 +0900

    Update kubebuilder from v4.2.0 to v4.3.0

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -28,7 +28,7 @@ var (
 	// GroupVersion is group version used to register these objects
 	GroupVersion = schema.GroupVersion{Group: "argocdcommenter.int128.github.io", Version: "v1"}
 
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
